2ce92833f792b14bb69a18cfa379117a60bf8586
priyadh1712 <99671393+priyadh1712@users.noreply.github.com>
2024-12-25 00:51:54 +0530

    Updated LogIn.jsx

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -31,13 +31,13 @@ const LogIn = () => {
 
       const data = await response.json();
 
-      if (response.ok) {
+       if (response.ok) {
         const { token, userId } = data;
-        localStorage.setItem("authToken", token);
+        localStorage.setItem("token", token);
         localStorage.setItem("userId", userId);
         setIsAuthenticated(true); 
         navigate("/");
-    }  else {
+      }else {
         throw new Error(data.message || "Failed to login.");
       }
     } catch (err) {
